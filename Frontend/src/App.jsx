8280a8d042fb785bea693bbdf308c28e160ecc1d
dshah-dev <dshah@trueigtech.com>
2026-01-13 12:03:33 +0530

    Practice of js basics

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,14 +1,11 @@
 import './App.css'
 import Task1 from './Components/task1'
 import Task2 from './Components/task2'
-import Task3 from './Components/task3'
 
 function App() {
   return (
     <>
-      {/* <Task1 />
-      <Task2 />
-      <Task3 />  */}
+
     </>
   )
 }
--- Frontend/src/Components/JsPractice.js
+++ Frontend/src/Components/JsPractice.js
@@ -1,7 +1,20 @@
-// console.log('[]',{});
+// console.log([]+{});
+console.log([]+{});
 const obj ={
     Name:"Dhaval",
     Age:21,
-    cgpa:9.1
+    cgpa:8.33
 };
-console.log();
+// console.log(obj);
+obj['cgpa'] = 9.3;
+// console.log(obj.Name);
+// console.log(obj["cgpa"]);
+
+const product ={
+    name:"pen",
+    rating:4,
+    offer:5+'%',
+    price:200
+}
+
+console.log(product)
